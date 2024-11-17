8ec988bc0bb4039871ce00ad7876b8a4cb84aa69
cotete <feliperibeirots@gmail.com>
2024-11-17 16:49:19 -0300

    feito o dropdown de industria

---
+++ src/components/DashboardComponents/DropdownIndustria/DropdownIndustria.tsx
@@ -0,0 +1,24 @@
+
+import { industriaFinal } from '@/utils/types/types';
+import React from 'react';
+
+type DropDownProps = {
+    label: string;
+    onChange: (valor: React.ChangeEvent<HTMLSelectElement>) => void;
+    industrias: industriaFinal[];
+}
+
+export default function DropDownIndustria({ label, onChange, industrias }: DropDownProps) {
+    return (
+        <div className="flex flex-col areaDropDown">
+            <label className="mt-3 font-semibold text-lg"> {label}</label>
+            <select className="rounded-xl p-3 border-2 border-primary shadow-xl" onChange={valor => onChange(valor)} name={label}>
+                {industrias.map(industria => (
+                    <option key={industria.idIndustria} value={industria.idIndustria}>
+                        {industria.nome}
+                    </option>
+                ))}
+            </select>
+        </div>
+    );
+}
