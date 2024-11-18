71bd7fa4e4727f7fee692fd48c46087ce7d49d18
JuliaAngelozi <jumangelozi@gmail.com>
2024-11-18 15:21:05 -0300

    imagens

--- src/app/empresas/page.tsx
+++ src/app/empresas/page.tsx
@@ -1,7 +1,10 @@
+import Sidebar from "@/components/Sidebar/Sidebar"
+
 export default function Empresas(){
     return(
         <>
             <h1>Parte empresas :)</h1>
+            <Sidebar></Sidebar>
         </>
     )
 }
