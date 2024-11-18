4c1f14099cfdbc44954b11f7850a80bd29f87c07
JuliaAngelozi <jumangelozi@gmail.com>
2024-11-18 17:07:36 -0300

    organizando icones

--- src/components/DashboardComponents/CardInfos/CardInfos.tsx
+++ src/components/DashboardComponents/CardInfos/CardInfos.tsx
@@ -12,7 +12,7 @@ export default function CardInfos({ image, alt, titulo, Informacao }: CardInfosP
     return (
         <div className='flex flex-col w-1/2 shadow-sm rounded-md'>
             <div className='w-full rounded-t-md h-[3.5rem] flex justify-center items-center gap-3 font-semibold bg-[#AA93B7] text-white'>
-                <Image src={image} alt={alt} width={40} height={40}></Image>
+                <Image src={image} alt={alt} width={30} height={30}></Image>
                 <h1>{titulo}</h1>
             </div>
             <div className='w-full font-semibold h-[3.5rem] flex justify-center items-center'>
